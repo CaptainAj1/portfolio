0139c03ee42f59540796acf5f7e079659d8f1970
CaptainAj1 <38005623+CaptainAj1@users.noreply.github.com>
2020-08-22 11:53:08 -0700

    Update index.php

--- index.php
+++ index.php
@@ -254,7 +254,7 @@
 				<div class="col-md-3 text-center col-padding animate-box">
 					<a href="https://youtu.be/YjLfA5SJBuI" target="_blank" class="work" style="background-image: url(images/prelude.png); text-align: center; float: right;">
 						<div class="desc">
-							<h3>Sample Villa Tour</h3>
+							<h3>Luxurious Villa Tour</h3>
 							<span>RealEstate Video</span>
 						</div>
 					</a>
